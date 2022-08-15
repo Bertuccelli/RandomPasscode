27cb72f3595be237fdfbca0a51bde57274818ca2
Amanda Bertuccelli <akbertuccelli@gmail.com>
2022-08-14 21:58:31 -0700

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Session;
+using Microsoft.AspNetCore.Mvc;
+using RandomPasscode.Models;
+
+namespace RandomPasscode.Controllers;
+
+public class HomeController : Controller {
+    [HttpGet("")]
+    public IActionResult Generate()
+    {
+        int? Count = HttpContext.Session.GetInt32("count");
+        if(Count == null){
+            HttpContext.Session.SetInt32("count", 1);
+        }
+        RandomPasscode newPass = new RandomPasscode();
+        return View("Index", newPass);
+    }
+
+    [HttpPost("/increment")]
+    public IActionResult AddOne()
+    {
+        int? totalPass = HttpContext.Session.GetInt32("count");
+        if(totalPass == null)
+        {
+            totalPass = 1;
+        } else {
+            totalPass += 1;
+        }
+        HttpContext.Session.SetInt32("count", (int)totalPass);
+        return RedirectToAction("Generate");
+    }
+}
